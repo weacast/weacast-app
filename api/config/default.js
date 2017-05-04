e0a13dedaad00655f3105628ce61e38282ba4347
Luc Claustres <luc.claustres@orange.fr>
2017-05-04 16:48:48 +0200

    Added lower limit for harvesting, bounds, size in config

--- api/config/default.js
+++ api/config/default.js
@@ -39,11 +39,14 @@ module.exports = {
       model: 'arpege',
       token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
       wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-05-GLOBE-WCS?SERVICE=WCS&version=2.0.1',
-      resolution: 0.5,
+      bounds: [-180, -90, 180, 90],
+      size: [720, 361],
+      resolution: [0.5, 0.5],
       runInterval: 6 * 3600,          // Produced every 6h
       oldestRunInterval: 24 * 3600,   // Don't go back in time older than 1 day
       interval: 3 * 3600,             // Steps of 3h
-      limit: 102 * 3600,              // Up to T0+102
+      lowerLimit: 0,                  // From T0
+      upperLimit: 102 * 3600,         // Up to T0+102
       updateInterval: 15 * 60,        // Check for update every 15 minutes
       elements: [
         {
@@ -67,11 +70,14 @@ module.exports = {
       model: 'arpege',
       token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
       wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-GLOBAL-ARPEGE-01-EUROPE-WCS?SERVICE=WCS&version=2.0.1',
-      resolution: 0.1,
+      bounds: [-32, 20, 42, 72],
+      size: [741, 721],
+      resolution: [0.1, 0.1],
       runInterval: 6 * 3600,            // Produced every 6h
       oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
       interval: 1 * 3600,               // Steps of 1h
-      limit: 102 * 3600,                // Up to T0+102
+      lowerLimit: 0,                    // From T0
+      upperLimit: 102 * 3600,           // Up to T0+102
       updateInterval: 15 * 60,          // Check for update every 15 minutes
       elements: [
         {
@@ -91,43 +97,18 @@ module.exports = {
       ]
     },
     {
-      name: 'arome-france-low',
+      name: 'arome-france',
       model: 'arome',
       token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
       wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-0025-FRANCE-WCS?SERVICE=WCS&version=2.0.1',
-      resolution: 0.025,
+      bounds: [-8, 38, 12, 53],
+      size: [801, 601],
+      resolution: [0.025, 0.025],
       runInterval: 3 * 3600,            // Produced every 3h
       oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
       interval: 1 * 3600,               // Steps of 1h
-      limit: 42 * 3600,                 // Up to T0+42
-      updateInterval: 15 * 60,          // Check for update every 15 minutes
-      elements: [
-        {
-          name: 'u-wind',
-          coverageid: 'U_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
-          subsets: {
-            height: 10
-          }
-        },
-        {
-          name: 'v-wind',
-          coverageid: 'V_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
-          subsets: {
-            height: 10
-          }
-        }
-      ]
-    },
-    {
-      name: 'arome-france-high',
-      model: 'arome',
-      token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
-      wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-001-FRANCE-WCS?SERVICE=WCS&version=2.0.1',
-      resolution: 0.025,
-      runInterval: 3 * 3600,            // Produced every 3h
-      oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
-      interval: 1 * 3600,               // Steps of 1h
-      limit: 42 * 3600,                 // Up to T0+42
+      lowerLimit: 0,                    // From T0
+      upperLimit: 102 * 3600,           // Up to T0+42
       updateInterval: 15 * 60,          // Check for update every 15 minutes
       elements: [
         {
@@ -146,6 +127,41 @@ module.exports = {
         }
       ]
     }
+    // This model generates too much data to be stored in MongoDB documents (limited to 16 MB)
+    /*
+    ,
+    {
+      name: 'arome-france-high',
+      model: 'arome',
+      token: '__qEMDoIC2ogPRlSoRQLGUBOomaxJyxdEd__',
+      wcsBaseUrl: 'https://geoservices.meteofrance.fr/services/MF-NWP-HIGHRES-AROME-001-FRANCE-WCS?SERVICE=WCS&version=2.0.1',
+      bounds: [-12, 37.5, 16, 55.4],
+      size: [2801, 1791],
+      resolution: 0.01,
+      runInterval: 3 * 3600,            // Produced every 3h
+      oldestRunInterval: 24 * 3600,     // Don't go back in time older than 1 day
+      interval: 1 * 3600,               // Steps of 1h
+      lowerLimit: 0,                    // From T0
+      upperLimit: 42 * 3600,            // Up to T0+42
+      updateInterval: 15 * 60,          // Check for update every 15 minutes
+      elements: [
+        {
+          name: 'u-wind',
+          coverageid: 'U_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
+          subsets: {
+            height: 10
+          }
+        },
+        {
+          name: 'v-wind',
+          coverageid: 'V_COMPONENT_OF_WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND',
+          subsets: {
+            height: 10
+          }
+        }
+      ]
+    }
+    */
   ]
 }
 
