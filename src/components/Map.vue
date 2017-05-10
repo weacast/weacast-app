c4edf36e58525376e0fcb7c9f5bc077984dd3f52
Luc Claustres <luc.claustres@orange.fr>
2017-05-10 12:36:13 +0200

    Updated code to include layers from core and fixed issues when updating forecast model

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -17,10 +17,12 @@ import 'leaflet-basemaps/L.Control.Basemaps.css'
 import 'leaflet-fullscreen/dist/Leaflet.fullscreen.js'
 import 'leaflet-fullscreen/dist/leaflet.fullscreen.css'
 import 'leaflet-filelayer'
-// import HeatmapOverlay from 'leaflet-heatmap'
 import 'leaflet-timedimension/dist/leaflet.timedimension.src.js'
 import 'leaflet-timedimension/dist/leaflet.timedimension.control.css'
-import WindLayer from 'src/WindLayer'
+
+import FlowLayer from 'weacast-core/lib/layers/flow-layer.js'
+// Heat layer does not work well to represent scalar value at low resolution due to lat/lon distortions
+// import HeatLayer from 'weacast-core/lib/layers/heat-layer.js'
 
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
@@ -38,8 +40,7 @@ export default {
   },
   watch: {
     forecastModel: function (model) {
-      // this.updateCloudCover()
-      this.updateWind()
+      this.setupForecastLayers()
     }
   },
   methods: {
@@ -48,86 +49,45 @@ export default {
         this.baseLayers.push(L[baseLayer.type](...baseLayer.arguments))
       })
     },
-    /* Does not seem to work well due to distortions in lat/lon
-    updateCloudCover () {
-      // Query cloud cover for current time
-      let query = {
-        query: {
-          time: new Date().toISOString(),
-          '$select': ['data']
-        }
-      }
+    setupForecastLayers () {
+      // Not yet ready
+      if (!this.forecastModel) return
 
-      api.service('/' + this.forecastModel.name + '/u-wind').find(query)
-      .then(response => {
-        let data = response.data[0].data
-        // Depending on the model longitude/latitude increases/decreases according to grid scanning
-        let lonDirection = (this.forecastModel.origin[0] === this.forecastModel.bounds[0] ? 1 : -1)
-        let latDirection = (this.forecastModel.origin[1] === this.forecastModel.bounds[1] ? 1 : -1)
-        let heatdata = []
-        for (let j = 0; j < this.forecastModel.size[1]; j++) {
-          for (let i = 0; i < this.forecastModel.size[0]; i++) {
-            let value = data[i + j * this.forecastModel.size[1]]
-            let lng = this.forecastModel.origin[0] + lonDirection * (i * this.forecastModel.resolution[0])
-            let lat = this.forecastModel.origin[1] + latDirection * (j * this.forecastModel.resolution[1])
-            heatdata.push({
-              lat,
-              lng,
-              value
-            })
-          }
-        }
-        this.cloudCover.setData({
-          data: heatdata,
-          max: 100
-        })
+      /*
+      if (this.temperature) {
+        this.map.removeLayer(this.temperature)
+      }
+      this.temperature = new HeatLayer({
+        element: 'temperature'
       })
-    },
-    */
-    updateWind () {
+      this.temperature.setForecastModel(this.forecastModel)
+      this.map.addLayer(this.temperature)
+      */
+      if (this.wind) {
+        this.map.removeLayer(this.wind)
+      }
+      this.wind = new FlowLayer({
+        uElement: 'u-wind',
+        vElement: 'v-wind'
+      })
+      this.map.addLayer(this.wind)
       this.wind.setForecastModel(this.forecastModel)
     }
   },
   mounted () {
     var map = L.map('map', {
       timeDimension: true,
-      timeDimensionControl: true
+      timeDimensionControl: true,
+      timeDimensionControlOptions: {
+        position: 'bottomright',
+        speedSlider: false,
+        playButton: false
+      }
     }).setView([46.578992, -0.294869], 10)
     this.map = map
 
     this.setupBaseLayers()
-
-    /*
-    this.cloudCover = new HeatmapOverlay({
-      // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-      // if scaleRadius is false it will be the constant radius used in pixels
-      radius: 0.75,
-      minOpacity: 0,
-      maxOpacity: 0.8,
-      // scales the radius based on map zoom
-      scaleRadius: true,
-      // custom gradient colors for cloud cover
-      gradient: {
-        // enter n keys between 0 and 1 here
-        // for gradient color customization
-        '0': 'black',
-        '1': 'white'
-      },
-      // if set to false the heatmap uses the global maximum for colorization
-      // if activated: uses the data maximum within the current map boundaries
-      //   (there will always be a red spot with useLocalExtremas true)
-      useLocalExtrema: true,
-      // which field name in your data represents the latitude - default "lat"
-      latField: 'lat',
-      // which field name in your data represents the longitude - default "lng"
-      lngField: 'lng',
-      // which field name in your data represents the data value - default "value"
-      valueField: 'value'
-    })
-    map.addLayer(this.cloudCover)
-    */
-    this.wind = new WindLayer()
-    map.addLayer(this.wind)
+    this.setupForecastLayers()
 
     var scaleControl = L.control.scale()
     scaleControl.addTo(map)
