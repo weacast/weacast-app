8c90d5319f131fe2e1f6defcb5ec73440b4e0e2a
Luc Claustres <luc.claustres@orange.fr>
2018-09-13 16:53:06 +0200

    Removed unused LevelUp/NeDB files/dependencies

--- api/src/models/users.model.levelup.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (app, options) {
-  options.db = app.db.collection('users')
-  // options.idField = '_id'
-}
--- api/src/models/users.model.nedb.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (app, options) {
-  options.Model = app.db.collection('users')
-  options.Model.ensureIndex({ fieldName: 'email', unique: true })
-}
