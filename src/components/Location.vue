0a1631a4c2bf0862b4c8505419bd079b588ecda7
Luc Claustres <luc.claustres@orange.fr>
2018-10-24 16:45:23 +0200

    Fixed issue in location probing with sparse data

--- src/components/Location.vue
+++ src/components/Location.vue
@@ -31,6 +31,7 @@
 
 <script>
 
+import _ from 'lodash'
 import moment from 'moment'
 import Chart from 'chart.js'
 
@@ -47,9 +48,11 @@ export default {
   computed: {
     location: function () {
       if (!this.feature) return ''
-      else return this.feature.properties.iata_code ?
-        this.feature.properties.iata_code :
-        this.feature.geometry.coordinates[0].toFixed(2) + '°, ' + this.feature.geometry.coordinates[1].toFixed(2) + '°'
+      else {
+        return this.feature.properties.iata_code
+        ? this.feature.properties.iata_code
+        : this.feature.geometry.coordinates[0].toFixed(2) + '°, ' + this.feature.geometry.coordinates[1].toFixed(2) + '°'
+      }
     }
   },
   methods: {
@@ -60,36 +63,47 @@ export default {
       // Destroy previous graph if any
       if (this.chart) this.chart.destroy()
       const color = Chart.helpers.color
+      const forecastTime = this.feature.forecastTime
+      const properties = this.feature.properties
+      // Make union of all available times for x-axis
+      let times = _.union(forecastTime.gust, forecastTime.windSpeed, forecastTime.precipitations)
+        .map(time => moment.utc(time))
+        .sort((a, b) => a - b)
+
       const config = {
         type: 'line',
         data: {
-          labels: this.feature.forecastTime.gust.map(time => new Date(time)),
+          labels: times,
           datasets: [{
             label: 'Wind gust',
             backgroundColor: color('rgb(255, 99, 132)').alpha(0.5).rgbString(),
             borderColor: 'rgb(255, 99, 132)',
             fill: false,
-            data: this.feature.properties.gust,
+            data: properties.gust.map((value, index) => ({ x: new Date(forecastTime.gust[index]), y: value })),
             yAxisID: 'speed'
           }, {
             label: 'Wind speed',
             backgroundColor: color('rgb(255, 159, 64)').alpha(0.5).rgbString(),
             borderColor: 'rgb(255, 159, 64)',
             fill: false,
-            data: this.feature.properties.windSpeed,
+            data: properties.windSpeed.map((value, index) => ({ x: new Date(forecastTime.windSpeed[index]), y: value })),
             yAxisID: 'speed'
           }, {
             label: 'Precipitations',
             backgroundColor: color('rgb(54, 162, 235)').alpha(0.5).rgbString(),
             borderColor: 'rgb(54, 162, 235)',
             fill: false,
-            data: this.feature.properties.precipitations,
+            data: properties.precipitations.map((value, index) => ({ x: new Date(forecastTime.precipitations[index]), y: value })),
             yAxisID: 'precipitations'
           }]
         },
         options: {
-          title: {
-            text: 'Local conditions'
+          tooltips: {
+            callbacks: {
+              label: (tooltipItem, data) => {
+                return data.datasets[tooltipItem.datasetIndex].label + ': ' + tooltipItem.yLabel.toFixed(2)
+              }
+            }
           },
           scales: {
             xAxes: [{
